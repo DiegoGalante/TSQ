aea765aa9c33b90e21b56f4e1e4ff1ca370dc863
Diego Galante <diegogalantte@gmail.com>
2022-08-06 18:39:58 -0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -5,9 +5,7 @@ namespace TSQ
     public static class Program
     {
         public static void Main()
-        {
-            Console.WriteLine($"Resultado da soma: {Somar(1, 1)}");
-        }
+        { }
 
         public static int Somar(int a, int b)
         {
